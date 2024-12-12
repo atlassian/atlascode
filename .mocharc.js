776a78b5b319197c5118c2aa5efeaa3b7b07f3ee
Bryan Wieger <bwieger@atlassian.com>
2024-12-12 10:48:59 -0800

    Simple working example axon-32

---
+++ .mocharc.js
@@ -0,0 +1,6 @@
+// increase default test case timeout to 5 seconds
+module.exports = {
+	timeout: 5000,
+	require: 'ts-node/register',
+	extension: ['ts'],
+};
---
+++ src/ui-test/simple.test.js
@@ -0,0 +1,25 @@
+let assert;
+// import the webdriver and the high level browser wrapper
+// import { before, VSBrowser, WebDriver } from "vscode-extension-tester";
+const before = require("vscode-extension-tester").before;
+const VSBrowser = require("vscode-extension-tester").VSBrowser;
+const WebDriver = require("vscode-extension-tester").WebDriver;
+
+// Create a Mocha suite
+describe("My Test Suite", () => {
+  let browser;
+  let driver;
+
+  // initialize the browser and webdriver
+  before(async () => {
+    assert = (await import("chai")).assert;
+    browser = VSBrowser.instance;
+    driver = browser.driver;
+  });
+
+  // test whatever we want using webdriver, here we are just checking the page title
+  it("My Test Case", async () => {
+    const title = await driver.getTitle();
+    assert.notEqual(title, "not-a-title");
+  });
+});
