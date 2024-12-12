7707d30aea83efecb0befd0d785b738bbb7897a9
Bryan Wieger <bwieger@atlassian.com>
2024-12-12 15:59:55 -0800

    ts test work

--- src/ui-test/dummy.test.ts
+++ src/ui-test/dummy.test.ts
@@ -1,18 +1,14 @@
-let assert;
 // import the webdriver and the high level browser wrapper
-// import { before, VSBrowser, WebDriver } from "vscode-extension-tester";
-const before = require("vscode-extension-tester").before;
-const VSBrowser = require("vscode-extension-tester").VSBrowser;
-const WebDriver = require("vscode-extension-tester").WebDriver;
+import { assert } from "chai";
+import { before, VSBrowser, WebDriver } from "vscode-extension-tester";
 
 // Create a Mocha suite
 describe("My Test Suite", () => {
-  let browser;
-  let driver;
+  let browser: VSBrowser;
+  let driver: WebDriver;
 
   // initialize the browser and webdriver
   before(async () => {
-    assert = (await import("chai")).assert;
     browser = VSBrowser.instance;
     driver = browser.driver;
   });
@@ -20,8 +16,6 @@ describe("My Test Suite", () => {
   // test whatever we want using webdriver, here we are just checking the page title
   it("My Test Case", async () => {
     const title = await driver.getTitle();
-
-    // reduces flakiness due to two files being opened
     assert.isTrue(title === "Getting Started" || title === "Walkthrough: Setup VS Code");
   });
 });
