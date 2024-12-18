a01b23be0f8967380c50b55982da1afd7bd04fd8
Bryan Wieger <bwieger@atlassian.com>
2024-12-18 15:49:28 -0800

    First iteration on e2e tests - no auth

--- src/react/atlascode/config/auth/CloudAuthButton.tsx
+++ src/react/atlascode/config/auth/CloudAuthButton.tsx
@@ -18,7 +18,7 @@ export const CloudAuthButton: React.FunctionComponent<CloudAuthButtonProps> = ({
     };
 
     return (
-        <Button variant="contained" color="primary" startIcon={<CloudIcon />} onClick={() => handleCloudProd()}>
+        <Button id="settings.login.cloud.button" variant="contained" color="primary" startIcon={<CloudIcon />} onClick={() => handleCloudProd()}>
             {loginText}
         </Button>
     );
