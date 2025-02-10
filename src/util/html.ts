48244f5e77e49f745da8e42f30802a086414ed3d
Marco Mura <mmura@atlassian.com>
2025-02-10 10:10:13 -0800

    Fix images in Jira Cloud tickets

---
+++ src/util/html.ts
@@ -0,0 +1,13 @@
+const regex1 = / src="\/[^"]+/g;
+const regex2 = / src='\/[^']+/g;
+
+export function replaceRelativeURLsWithAbsolute(renderedHtml: string, baseApiUrl: string): string | undefined {
+    if (!renderedHtml || !baseApiUrl) {
+        return renderedHtml;
+    }
+    
+    // substring(7) because we need to get the relative url without the first '/'
+    return renderedHtml
+        .replace(regex1, x => ` src=\"${new URL(x.substring(7), baseApiUrl).href}`)
+        .replace(regex2, x => ` src=\'${new URL(x.substring(7), baseApiUrl).href}`);
+}
