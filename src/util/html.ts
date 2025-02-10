82c296f99db470438b2c04c108ecfb51d3a980cf
Marco Mura <mmura@atlassian.com>
2025-02-10 10:43:47 -0800

    rewrote comment to make it clearer

--- src/util/html.ts
+++ src/util/html.ts
@@ -6,7 +6,9 @@ export function replaceRelativeURLsWithAbsolute(renderedHtml: string, baseApiUrl
         return renderedHtml;
     }
 
-    // substring(7) because we need to get the relative url without the first '/'
+    // The regex is searching for anything starting with ' src="/' which is 7 chars long,
+    // and we need to get the relative URL without including its first /, so anything after those 7 characters.
+    // Therefore, substring(7).
     return renderedHtml
         .replace(regex1, (x) => ` src=\"${new URL(x.substring(7), baseApiUrl).href}`)
         .replace(regex2, (x) => ` src=\'${new URL(x.substring(7), baseApiUrl).href}`);
