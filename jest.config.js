27f2aa80463d81b97c1d3cf8f20bc53b3f3ad269
Marco Mura <mmura@atlassian.com>
2025-03-17 16:20:38 -0700

    Refactor Jest configuration to minimize differences between unit/react

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,35 @@
+const _baseConfig = (project, testExtension) => ({
+    displayName: project,
+    roots: ['<rootDir>'],
+    
+    testMatch: [`**/*.test.${testExtension}`],
+    testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+
+    transform: {
+        '^.+\\.(js|ts|tsx)$': [
+            'ts-jest',
+            {
+                tsconfig: project === 'react' ? ({ esModuleInterop: true }) : false,
+                isolatedModules: project === 'react',
+            },
+        ],
+    },
+    transformIgnorePatterns: ['/node_modules/(?!(@vscode/webview-ui-toolkit/|@microsoft/|exenv-es6/))'],
+
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/*.{spec,test}.{ts,tsx,js,jsx}', // Exclude test files
+    ],
+    coverageDirectory: `coverage/${project}`,
+    coverageReporters: ['json', 'lcov', 'text-summary', 'clover'],
+});
+
 module.exports = {
-    projects: ['<rootDir>/jest.react.config.js', '<rootDir>/jest.unit.config.js'],
+    projects: ['<rootDir>/jest.*.config.js'],
     verbose: true,
+
+    // custom exports for individual projects
+    _baseConfig,
 };
