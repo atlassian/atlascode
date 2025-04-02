4c1be6d028e95355dedd05aabd543b82b86a8282
Marco Mura <mmura@atlassian.com>
2025-04-02 15:11:20 -0700

    remove Error override

--- src/errorReporting.ts
+++ src/errorReporting.ts
@@ -28,61 +28,6 @@ function errorHandler(error: Error): void {
     } catch {}
 }
 
-const builtInErrorInOverriddenErrorPropertyKey = '__atlascode_originalError';
-
-function replaceJavascriptErrorObject() {
-    // if we are calling this twice, probably it's because the extension got upgraded and it's re-initializing.
-    // we need to retrieve the original Error and re-create another override
-    const originalError = Error.hasOwnProperty(builtInErrorInOverriddenErrorPropertyKey)
-        ? ((Error as any)[builtInErrorInOverriddenErrorPropertyKey] as ErrorConstructor)
-        : Error;
-
-    const tmp = class Error extends originalError {
-        constructor(message?: string, ...args: any[]) {
-            super(message, ...args);
-
-            // for Errors created in the wild, we filter for the string `atlascode` to avoid noise
-            if (message && message.includes('atlascode')) {
-                errorHandler(this as any);
-            }
-        }
-    };
-
-    // this because Error can be invoked with or without 'new' to create a new instance of it
-    // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/Error
-    Error = function (...args) {
-        return new tmp(...args);
-    } as ErrorConstructor;
-
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/captureStackTrace
-    if (originalError.hasOwnProperty('captureStackTrace')) {
-        Error.captureStackTrace = originalError.captureStackTrace;
-    }
-
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/isError
-    if (originalError.hasOwnProperty('isError')) {
-        (Error as any).isError = (originalError as any).isError;
-    }
-
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/stackTraceLimit
-    if (originalError.hasOwnProperty('stackTraceLimit')) {
-        Object.defineProperty(Error, 'stackTraceLimit', {
-            get: () => originalError.stackTraceLimit,
-            set: (value: number) => (originalError.stackTraceLimit = value),
-            enumerable: false,
-            configurable: true,
-        });
-    }
-
-    (Error as any)[builtInErrorInOverriddenErrorPropertyKey] = originalError;
-}
-
-function restoreJavascriptErrorObject() {
-    if (Error.hasOwnProperty(builtInErrorInOverriddenErrorPropertyKey)) {
-        Error = (Error as any)[builtInErrorInOverriddenErrorPropertyKey] as ErrorConstructor;
-    }
-}
-
 export function registerErrorReporting(): void {
     if (!nodeJsErrorReportingRegistered) {
         nodeJsErrorReportingRegistered = true;
@@ -90,7 +35,6 @@ export function registerErrorReporting(): void {
         try {
             process.addListener('uncaughtException', errorHandler);
             Logger.addListener('error', errorHandler);
-            replaceJavascriptErrorObject();
         } catch {}
     }
 }
@@ -99,7 +43,6 @@ export function unregisterErrorReporting(): void {
     try {
         process.removeListener('uncaughtException', errorHandler);
         Logger.removeListener('error', errorHandler);
-        restoreJavascriptErrorObject();
         nodeJsErrorReportingRegistered = false;
     } catch {}
 }
