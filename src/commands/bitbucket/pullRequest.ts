70c84a887c79aa1b923cc6b22631b9ebd0c7ad2e
Parth Jadhav <pjadhav@atlassian.com>
2025-04-04 17:31:50 +0530

    chore: update name of fn

--- src/commands/bitbucket/pullRequest.ts
+++ src/commands/bitbucket/pullRequest.ts
@@ -1,7 +1,7 @@
 import { Uri } from 'vscode';
 import { CheckoutHelper } from 'src/bitbucket/interfaces';
 
-const parsePrUrl = (url: string): { repoUrl: string; prId: number } => {
+const extractPullRequestComponents = (url: string): { repoUrl: string; prId: number } => {
     const repoUrl = url.slice(0, url.indexOf('/pull-requests'));
     const prUrlPath = Uri.parse(url).path;
     const prId = prUrlPath.slice(prUrlPath.lastIndexOf('/') + 1);
@@ -13,6 +13,6 @@ const parsePrUrl = (url: string): { repoUrl: string; prId: number } => {
  * @param pullRequestUrl URL of the pull request to open
  */
 export const openPullRequest = async (bitbucketHelper: CheckoutHelper, pullRequestUrl: string): Promise<void> => {
-    const { repoUrl, prId } = parsePrUrl(pullRequestUrl);
+    const { repoUrl, prId } = extractPullRequestComponents(pullRequestUrl);
     await bitbucketHelper.pullRequest(repoUrl, prId);
 };
