d27c6e2b5b48f5cdea16a5bcd95ddde76b368567
Christian Abella <cabella@atlassian.com>
2025-04-23 10:27:47 -1000

    hackathon: able to reference specific webview html

--- e2e/tests/playwright.spec.ts
+++ e2e/tests/playwright.spec.ts
@@ -1,20 +1,16 @@
 import { expect, test } from '@playwright/test';
 
 test('Jira login flow', async ({ page }) => {
-    await page.goto('http://0.0.0.0:9988/');
+    await page.goto('http://127.0.0.1:9988/');
 
     await page.getByRole('tab', { name: 'Atlassian' }).click();
     await page.getByText('Please login to Jira').click();
 
     await expect(page.getByRole('tab', { name: 'Atlassian Settings' })).toBeVisible();
 
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(3000);
 
-    // await expect(page.getByRole('button', { name: /login to jira/i })).toBeVisible();
+    const webview = page.mainFrame().childFrames()[2].childFrames()[0];
 
-    //   await page.getByLabel(/login to jira/i).click()
-
-    //   await expect(page.getByText('Add Jira Site')).toBeVisible();
-
-    //   await expect(page).toHaveScreenshot();
+    await expect(webview.getByRole('button', { name: 'Login to Jira' })).toBeVisible();
 });
