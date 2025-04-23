a17c413a13ca06b2f38d12db40277c7225789d16
Stan Dzhumaev <sdzhumaev@atlassian.com>
2025-04-23 14:34:50 -0700

    [hackathon] chore: inject interceptor to get API call data

--- src/atlclients/interceptors.ts
+++ src/atlclients/interceptors.ts
@@ -1,23 +1,84 @@
 import { AxiosInstance } from 'axios';
-import curlirize from 'axios-curlirize';
+import fs from 'fs';
+import path from 'path';
+// import curlirize from 'axios-curlirize';
 import { parse } from 'url';
+import * as vscode from 'vscode';
 
 import { Logger } from '../logger';
 
-interface CurlResult {
-    command: string;
-    object: any;
-}
+// interface CurlResult {
+//     command: string;
+//     object: any;
+// }
 export function addCurlLogging(transport: AxiosInstance): void {
-    curlirize(transport, (result: CurlResult, err: any) => {
-        let { command } = result;
-        command = command.replace('-H "Accept-Encoding:gzip, deflate" ', '');
-        if (!err) {
-            Logger.debug('-'.repeat(70));
-            Logger.debug(command);
-            Logger.debug('-'.repeat(70));
-        }
-    });
+    dumpEverythingIntoFile(transport);
+    // curlirize(transport, (result: CurlResult, err: any) => {
+    //     let { command } = result;
+    //     command = command.replace('-H "Accept-Encoding:gzip, deflate" ', '');
+    //     if (!err) {
+    //         Logger.debug('-'.repeat(70));
+    //         Logger.debug(command);
+    //         Logger.debug('-'.repeat(70));
+    //     }
+    // });
+}
+
+export function dumpEverythingIntoFile(transport: AxiosInstance): void {
+    // get vscode workspace folder
+    const workspaceRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+    if (!workspaceRoot) {
+        Logger.warn('No workspace root found. Cannot create call map file.');
+        return;
+    }
+    const callMapFile = path.join(workspaceRoot, 'atlascodeCalls.json');
+
+    transport.interceptors.response.use(
+        (config) => {
+            if (!fs.existsSync(callMapFile)) {
+                fs.writeFileSync(callMapFile, JSON.stringify({}));
+            }
+
+            try {
+                const fileContent = fs.readFileSync(callMapFile, 'utf8');
+                const jsonContent = JSON.parse(fileContent);
+
+                const url = config.request.path
+                    .split('?')[0]
+                    .replace(/[0-9a-fA-F-]{36}\//g, 'dummy-uuid/')
+                    .replace(/[0-9a-fA-F-]{36}-/g, 'dummy-uuid-');
+                const method = config.request.method;
+                const headers = config.headers;
+                const data = config.data;
+
+                jsonContent[url] = {
+                    method,
+                    headers,
+                    data,
+                };
+
+                fs.writeFileSync(callMapFile, JSON.stringify(jsonContent, null, 2));
+            } catch (error) {
+                console.error('Error reading or writing file:', error);
+            }
+
+            return config;
+        },
+        (error) => {
+            // Do something with request error
+            return Promise.reject(error);
+        },
+    );
+    transport.interceptors.response.use(
+        function (response) {
+            Logger.debug('Response:', response);
+            return response;
+        },
+        function (error) {
+            // Do something with response error
+            return Promise.reject(error);
+        },
+    );
 }
 
 /*
