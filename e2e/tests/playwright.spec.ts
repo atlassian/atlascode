19f983b5cca94941c69df9939cf1e6696f26be7f
Marco Mura <mmura@atlassian.com>
2025-05-10 16:27:57 -0700

    More mocked data, finalized authentication test

--- e2e/tests/playwright.spec.ts
+++ e2e/tests/playwright.spec.ts
@@ -66,62 +66,61 @@ test('Onboarding flow', async ({ page }) => {
     await expect(getStartedFrame.getByRole('heading', { name: "You're ready to get started!" })).toBeVisible();
 });
 
-test('everything else', async ({ page }) => {
+test('Authenticating with Jira works, and assigned items are displayed', async ({ page }) => {
     await page.setViewportSize({ width: 1600, height: 800 });
     await page.goto('http://[::1]:9988/');
 
     await page.getByRole('tab', { name: 'Atlassian' }).click();
+    await page.waitForTimeout(250);
 
     // Close the onboarding view
-
     await page.getByRole('tab', { name: 'Getting Started' }).getByLabel(/close/i).click();
 
-    /**
-     * Settings page
-     */
-
     await page.getByRole('treeitem', { name: 'Please login to Jira' }).click();
+    await page.waitForTimeout(250);
 
     await expect(page.getByRole('tab', { name: 'Atlassian Settings' })).toBeVisible();
 
     await page.getByRole('tab', { name: 'Atlassian Settings' }).click();
+    await page.waitForTimeout(250);
 
     const settingsFrame = page.frameLocator('iframe.webview').frameLocator('iframe[title="Atlassian Settings"]');
 
     await expect(settingsFrame.getByRole('button', { name: 'Authentication authenticate' })).toBeVisible();
-
-    //await expect(page).toHaveScreenshot({ fullPage: true });
-
     await expect(settingsFrame.getByRole('button', { name: 'Login to Jira' })).toBeVisible();
 
     settingsFrame.getByRole('button', { name: 'Login to Jira' }).click();
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(250);
 
     await settingsFrame.getByRole('textbox', { name: 'Base URL' }).click();
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(250);
 
     await settingsFrame.getByRole('textbox', { name: 'Base URL' }).fill('https://mockedteams.atlassian.net');
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(250);
 
     await settingsFrame.getByRole('textbox', { name: 'Username' }).click();
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(250);
 
     await settingsFrame.getByRole('textbox', { name: 'Username' }).fill('mock@atlassian.code');
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(250);
 
     await settingsFrame.getByRole('textbox', { name: 'Password (API token)' }).click();
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(250);
 
     await settingsFrame.getByRole('textbox', { name: 'Password (API token)' }).fill('12345');
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(250);
 
     await settingsFrame.getByRole('button', { name: 'Save Site' }).click();
+    await page.waitForTimeout(250);
 
-    await page.waitForTimeout(5000);
+    // wait a longer amount of time for site and treeview to initialize and render
+    await page.waitForTimeout(2000);
+    await expect(
+        settingsFrame
+            .getByRole('region', { name: 'Authentication authenticate' })
+            .getByText('mockedteams.atlassian.net'),
+    ).toBeVisible();
+    await expect(page.getByRole('treeitem', { name: 'BTS-1 - User Interface Bugs' })).toBeVisible();
 
-    // await page.getByRole('treeitem', { name: /authenticate with bitbucket/i }).click();
-
-    // await expect(settingsFrame.getByRole('button', { name: 'Login to Bitbucket' })).toBeVisible();
-
-    // await expect(page).toHaveScreenshot();
+    //await expect(page).toHaveScreenshot();
 });
