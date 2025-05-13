38473544b7f80224bd5049889e33ceeb342be03e
Bryan Wieger <bwieger@atlassian.com>
2025-05-13 10:48:14 -0700

    Basic auth banner working.

--- src/views/notifications/bannerDelegate.ts
+++ src/views/notifications/bannerDelegate.ts
@@ -1,11 +1,14 @@
-import { window } from 'vscode';
+import { Commands } from 'src/commands';
+import { commands, window } from 'vscode';
 
 import {
+    AtlasCodeNotification,
     NotificationAction,
     NotificationChangeEvent,
     NotificationDelegate,
     NotificationManagerImpl,
     NotificationSurface,
+    NotificationType,
 } from './notificationManager';
 
 export class BannerDelegate implements NotificationDelegate {
@@ -60,39 +63,87 @@ export class BannerDelegate implements NotificationDelegate {
     }
 
     private aggregateAndShowNotifications() {
-        this.showNotification(
-            'hi!',
-            'Yes',
-            () => {},
-            'Dismiss',
-            () => {},
-        );
+        // get each notification in the pile
+        // and show the notification
+        this.pile.forEach((event) => {
+            if (event.action === NotificationAction.Added) {
+                event.notifications.forEach((notification) => {
+                    this.showNotification(
+                        notification.message,
+                        this.makeActionText(notification),
+                        this.makeActionFunction(notification),
+                        this.makeDismissText(notification),
+                        this.makeDismissFunction(notification),
+                    );
+                });
+            }
+        });
     }
 
     private showNotification(
         message: string,
         yesText: string,
         yesAction: () => void,
-        dismissText: string,
-        dismissAction: () => void,
+        dismissText?: string,
+        dismissAction?: () => void,
     ) {
-        const manageNotifications = 'Manage Notifications';
-        const manageNotificationsAction = () => {};
-        window.showInformationMessage(message, yesText, dismissText, manageNotifications).then((selection) => {
+        let displayedNotification;
+        if (dismissText) {
+            displayedNotification = window.showInformationMessage(message, yesText, dismissText);
+        } else {
+            displayedNotification = window.showInformationMessage(message, yesText);
+        }
+
+        displayedNotification.then((selection) => {
             switch (selection) {
                 case yesText:
                     yesAction();
                     break;
                 case dismissText:
-                    dismissAction();
-                    break;
-                case manageNotifications:
-                    manageNotificationsAction();
+                    dismissAction?.();
                     break;
                 default:
                     break;
             }
         });
+    }
+
+    private makeActionText(notification: AtlasCodeNotification): string {
+        switch (notification.notificationType) {
+            case NotificationType.LoginNeeded:
+                return 'Log in to Jira';
+            default:
+                throw new Error(`Cannot make action text: Unknown notification type: ${notification.notificationType}`);
+        }
+    }
+
+    private makeDismissText(notification: AtlasCodeNotification): string | undefined {
+        switch (notification.notificationType) {
+            case NotificationType.LoginNeeded:
+                return 'Manage Notifications';
+            default:
+                return undefined;
+        }
+    }
+
+    private makeActionFunction(notification: AtlasCodeNotification): () => void {
+        switch (notification.notificationType) {
+            case NotificationType.LoginNeeded:
+                return () => {
+                    commands.executeCommand(Commands.ShowJiraAuth);
+                };
+            default:
+                throw new Error(`Cannot make action text: Unknown notification type: ${notification.notificationType}`);
+        }
+    }
+
+    private makeDismissFunction(notification: AtlasCodeNotification): undefined | (() => void) {
+        switch (notification.notificationType) {
+            case NotificationType.LoginNeeded:
+                return () => {};
+            default:
+                return undefined;
+        }
     }
 
     // private analytics(uri: Uri, count: number) {
