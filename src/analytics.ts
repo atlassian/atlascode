c11fca100d5eb64c2d4910c2e2616918fd560d26
Stan Dzhumaev <sdzhumaev@atlassian.com>
2025-05-29 11:24:51 -0700

    [AXON-432] Transition issue from the context menu

--- src/analytics.ts
+++ src/analytics.ts
@@ -198,8 +198,12 @@ export async function issueCreatedEvent(site: DetailedSiteInfo, issueKey: string
     return instanceTrackEvent(site, 'created', 'issue', { actionSubjectId: issueKey });
 }
 
-export async function issueTransitionedEvent(site: DetailedSiteInfo, issueKey: string): Promise<TrackEvent> {
-    return instanceTrackEvent(site, 'transitioned', 'issue', { actionSubjectId: issueKey });
+export async function issueTransitionedEvent(
+    site: DetailedSiteInfo,
+    issueKey: string,
+    method?: string,
+): Promise<TrackEvent> {
+    return instanceTrackEvent(site, 'transitioned', 'issue', { actionSubjectId: issueKey, method });
 }
 
 export async function issueUrlCopiedEvent(): Promise<TrackEvent> {
