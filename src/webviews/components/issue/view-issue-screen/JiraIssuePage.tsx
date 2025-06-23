cf9d149b7cd974f9a51fa93e4934cb88d08110dd
Andriy Vuy <avuy@atlassian.com>
2025-06-23 15:53:35 +0300

    Add ... while loading top parent of the hierarchy

--- src/webviews/components/issue/view-issue-screen/JiraIssuePage.tsx
+++ src/webviews/components/issue/view-issue-screen/JiraIssuePage.tsx
@@ -40,6 +40,7 @@ type Accept = CommonEditorPageAccept | EditIssueData;
 export interface ViewState extends CommonEditorViewState, EditIssueData {
     showMore: boolean;
     currentInlineDialog: string;
+    hierarchyLoading: boolean;
 }
 
 const emptyState: ViewState = {
@@ -47,6 +48,7 @@ const emptyState: ViewState = {
     ...emptyEditIssueData,
     showMore: false,
     currentInlineDialog: '',
+    hierarchyLoading: false,
 };
 
 export default class JiraIssuePage extends AbstractIssueEditorPage<Emit, Accept, {}, ViewState> {
@@ -114,6 +116,13 @@ export default class JiraIssuePage extends AbstractIssueEditorPage<Emit, Accept,
                 case 'hierarchyUpdate':
                     this.setState({
                         hierarchy: e.hierarchy,
+                        hierarchyLoading: false,
+                    });
+                    break;
+                case 'hierarchyLoading':
+                    this.setState({
+                        hierarchy: e.hierarchy,
+                        hierarchyLoading: true,
                     });
                     break;
                 case 'error':
@@ -475,6 +484,13 @@ export default class JiraIssuePage extends AbstractIssueEditorPage<Emit, Accept,
                     <div className="ac-breadcrumbs">
                         {this.state.hierarchy && this.state.hierarchy.length > 0 && (
                             <>
+                                {/* Show loading indicator at the beginning if we're fetching more parents */}
+                                {this.state.hierarchyLoading && (
+                                    <>
+                                        <span className="ac-breadcrumb-loading">...</span>
+                                        <span className="ac-breadcrumb-divider">/</span>
+                                    </>
+                                )}
                                 {this.state.hierarchy.map((issue, index) => {
                                     const isLastItem = index === this.state.hierarchy.length - 1;
 
