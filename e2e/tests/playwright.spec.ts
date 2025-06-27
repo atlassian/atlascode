83d42227b36a6b82810ec9217986e48f23a0521e
bhulovatyi <bhulovatyi@atlassian.com>
2025-06-27 11:27:38 +0300

    AXON-460: Added Test upload file

--- e2e/tests/playwright.spec.ts
+++ e2e/tests/playwright.spec.ts
@@ -119,3 +119,57 @@ test('Authenticating with Jira works, and assigned items are displayed', async (
 
     //await expect(page).toHaveScreenshot();
 });
+
+test('Test upload file', async ({ page }) => {
+    const oldDescription = 'Track and resolve bugs related to the user interface.';
+
+    await page.goto('http://localhost:9988/');
+
+    await page.getByRole('tab', { name: 'Atlassian' }).click();
+
+    await page.getByRole('tab', { name: 'Getting Started' }).getByLabel(/close/i).click();
+
+    await page.getByRole('treeitem', { name: 'Please login to Jira' }).click();
+
+    await page.getByRole('tab', { name: 'Atlassian Settings' }).click();
+
+    const settingsFrame = page.frameLocator('iframe.webview').frameLocator('iframe[title="Atlassian Settings"]');
+
+    await settingsFrame.getByRole('button', { name: 'Login to Jira' }).click();
+    await page.waitForTimeout(250);
+
+    await settingsFrame.getByRole('textbox', { name: 'Base URL' }).fill('https://mockedteams.atlassian.net');
+    await page.waitForTimeout(250);
+
+    await settingsFrame.getByRole('textbox', { name: 'Username' }).fill('mock@atlassian.code');
+    await page.waitForTimeout(250);
+
+    await settingsFrame.getByRole('textbox', { name: 'Password (API token)' }).fill('12345');
+    await page.waitForTimeout(250);
+
+    await settingsFrame.getByRole('button', { name: 'Save Site' }).click();
+    await page.waitForTimeout(2000);
+
+    await page.getByRole('treeitem', { name: 'BTS-1 - User Interface Bugs' }).click();
+    await page.waitForTimeout(250);
+
+    await page.getByRole('tab', { name: 'Atlassian Settings' }).getByLabel(/close/i).click();
+
+    const frameHandle = await page.frameLocator('iframe.webview').locator('iframe[title="Jira Issue"]').elementHandle();
+
+    if (!frameHandle) {
+        throw new Error('iframe element not found');
+    }
+    const issueFrame = await frameHandle.contentFrame();
+
+    if (!issueFrame) {
+        throw new Error('iframe element not found');
+    }
+
+    await issueFrame.waitForLoadState('domcontentloaded');
+
+    await expect(issueFrame.locator('body')).toBeVisible({ timeout: 15000 });
+
+    // Check the existing description
+    await expect(issueFrame.getByText(oldDescription)).toBeVisible({ timeout: 50000 });
+});
