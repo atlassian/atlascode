4f727ee63351eb242261e1e7d377eb35fd14e984
Migelangel Carrillo <migelangelcarrillo2@gmail.com>
2025-07-06 15:49:46 -0700

    Tested a new version of editIssueUI to accept cache
    Created and filled out the new parameters of the new editIssueUI function that accepts cache values. The new function is experimental and only tested via observation of Charles web proxy yet yielded good results after cache was populated.

--- src/jira/fetchIssue.ts
+++ src/jira/fetchIssue.ts
@@ -1,26 +1,25 @@
-import { createIssueUI, EditIssueUI, editIssueUI } from '@atlassianlabs/jira-metaui-client'; // add back editIssueUI when we change jira-metaui-client
+import { createIssueUI, EditIssueUI } from '@atlassianlabs/jira-metaui-client'; // add back editIssueUI when we change jira-metaui-client
+// // Extra imports to test modifiedEditIssueUI
+import { EditIssueScreenTransformer } from '@atlassianlabs/jira-metaui-transformer';
 import { DEFAULT_API_VERSION } from '@atlassianlabs/jira-pi-client';
+import { JiraClient } from '@atlassianlabs/jira-pi-client';
 import {
     isMinimalIssue,
     MinimalIssue,
     minimalIssueFromJsonObject,
     MinimalORIssueLink,
 } from '@atlassianlabs/jira-pi-common-models';
+import { IssueLinkType } from '@atlassianlabs/jira-pi-common-models';
+import { getEpicFieldInfo } from '@atlassianlabs/jira-pi-common-models';
+import { JiraSiteInfo } from '@atlassianlabs/jira-pi-common-models';
 import { CreateMetaTransformerResult } from '@atlassianlabs/jira-pi-meta-models';
+import { IssueCreateMetadata } from '@atlassianlabs/jira-pi-meta-models';
+import { Fields, readField } from '@atlassianlabs/jira-pi-meta-models';
+import { FieldTransformerResult } from '@atlassianlabs/jira-pi-meta-models';
 
 import { DetailedSiteInfo } from '../atlclients/authInfo';
 import { Container } from '../container';
 import { SearchJiraHelper } from '../views/jira/searchJiraHelper';
-
-// // Extra imports to test modifiedEditIssueUI
-// import { EditIssueScreenTransformer } from '@atlassianlabs/jira-metaui-transformer';
-// import { JiraClient } from '@atlassianlabs/jira-pi-client';
-// import { IssueLinkType } from '@atlassianlabs/jira-pi-common-models';
-// import { getEpicFieldInfo } from '@atlassianlabs/jira-pi-common-models';
-// import { JiraSiteInfo } from '@atlassianlabs/jira-pi-common-models';
-// import { IssueCreateMetadata } from '@atlassianlabs/jira-pi-meta-models';
-// import { Fields, readField } from '@atlassianlabs/jira-pi-meta-models';
-// import { FieldTransformerResult } from '@atlassianlabs/jira-pi-meta-models';
 
 export async function fetchCreateIssueUI(
     siteDetails: DetailedSiteInfo,
@@ -62,110 +61,109 @@ export async function fetchMinimalIssue(
 
 export async function fetchEditIssueUI(issue: MinimalIssue<DetailedSiteInfo>): Promise<EditIssueUI<DetailedSiteInfo>> {
     const client = await Container.clientManager.jiraClient(issue.siteDetails);
-    // const cMeta = await Container.jiraSettingsManager.getIssueCreateMetadata(
-    //     issue.key.substring(0, issue.key.indexOf('-')), // Project Key
-    //     issue.siteDetails,
-    // );
-    // const fields = await Container.jiraSettingsManager.getAllFieldsForSite(issue.siteDetails);
-    // const issuelinkTypes = await Container.jiraSettingsManager.getIssueLinkTypes(issue.siteDetails);
+    const cMeta = await Container.jiraSettingsManager.getIssueCreateMetadata(
+        issue.key.substring(0, issue.key.indexOf('-')), // Project Key
+        issue.siteDetails,
+    );
+    const fields = await Container.jiraSettingsManager.getAllFieldsForSite(issue.siteDetails);
+    const issuelinkTypes = await Container.jiraSettingsManager.getIssueLinkTypes(issue.siteDetails);
 
     // Lets do the cMeta call here so we can cache it
     // Still need to finish getting all the calls here lol
-    return await editIssueUI(issue, client, DEFAULT_API_VERSION); // This is the regular version for the function call
-    // return await modifiedEditIssueUI(issue, client, DEFAULT_API_VERSION, fields, issuelinkTypes, cMeta);
+    // return await editIssueUI(issue, client, DEFAULT_API_VERSION); // This is the regular version for the function call
+    return await modifiedEditIssueUI(issue, client, DEFAULT_API_VERSION, fields, issuelinkTypes, cMeta);
 }
 
-// async function modifiedEditIssueUI<S extends JiraSiteInfo>(
-//     issue: MinimalIssue<S>,
-//     client: JiraClient<S>,
-//     apiVersion: string = '2',
-//     fields: Fields = {},
-//     cachedIssueLinkTypes: IssueLinkType[] = [],
-//     cachedCMeta: IssueCreateMetadata = { projects: [] },
-// ): Promise<EditIssueUI<S>> {
-//     console.log('creating edit ui...');
-//     const projectKey = issue.key.substring(0, issue.key.indexOf('-'));
-//     const [allFields, issuelinkTypes, cMeta, issueResp] = await Promise.all([
-//         getFieldsInfo(client, fields),
-//         getIssueLinkTypesInfo(client, cachedIssueLinkTypes),
-//         getCreateIssueMetadataInfo(client, projectKey, cachedCMeta),
-//         client.getIssue(issue.key, ['*all'], 'transitions,renderedFields,editmeta,transitions.fields'),
-//     ]); // Added some parallel HTTP requests for preformace.
+async function modifiedEditIssueUI<S extends JiraSiteInfo>(
+    issue: MinimalIssue<S>,
+    client: JiraClient<S>,
+    apiVersion: string = '2',
+    fields: Fields = {},
+    cachedIssueLinkTypes: IssueLinkType[] = [],
+    cachedCMeta: IssueCreateMetadata = { projects: [] },
+): Promise<EditIssueUI<S>> {
+    console.log('creating edit ui...');
+    const projectKey = issue.key.substring(0, issue.key.indexOf('-'));
+    const [allFields, issuelinkTypes, cMeta, issueResp] = await Promise.all([
+        getFieldsInfo(client, fields),
+        getIssueLinkTypesInfo(client, cachedIssueLinkTypes),
+        getCreateIssueMetadataInfo(client, projectKey, cachedCMeta),
+        client.getIssue(issue.key, ['*all'], 'transitions,renderedFields,editmeta,transitions.fields'),
+    ]); // Added some parallel HTTP requests for preformace....
 
-//     const epicInfo = getEpicFieldInfo(allFields);
-//     const transformer: EditIssueScreenTransformer<S> = new EditIssueScreenTransformer(
-//         issue.siteDetails,
-//         apiVersion,
-//         allFields,
-//         issuelinkTypes,
-//     );
+    const epicInfo = getEpicFieldInfo(allFields);
+    const transformer: EditIssueScreenTransformer<S> = new EditIssueScreenTransformer(
+        issue.siteDetails,
+        apiVersion,
+        allFields,
+        issuelinkTypes,
+    );
 
-//     const epicNameProvider = async (key: string) => {
-//         const epicIssue = await client.getIssue(key, ['summary', epicInfo.epicName.id]);
-//         if (epicIssue && epicIssue.fields && epicIssue.fields[epicInfo.epicName.id]) {
-//             return epicIssue.fields[epicInfo.epicName.id];
-//         }
+    const epicNameProvider = async (key: string) => {
+        const epicIssue = await client.getIssue(key, ['summary', epicInfo.epicName.id]);
+        if (epicIssue && epicIssue.fields && epicIssue.fields[epicInfo.epicName.id]) {
+            return epicIssue.fields[epicInfo.epicName.id];
+        }
 
-//         return undefined;
-//     };
-//     const result: FieldTransformerResult = await transformer.transformIssue(issueResp, cMeta, epicNameProvider);
-//     return {
-//         ...result,
-//         key: issue.key,
-//         id: issue.id,
-//         self: issue.self,
-//         siteDetails: issue.siteDetails,
-//         isEpic: issue.isEpic,
-//         epicChildren: issue.epicChildren,
-//         apiVersion: apiVersion,
-//         epicFieldInfo: epicInfo,
-//     };
-// }
+        return undefined;
+    };
+    const result: FieldTransformerResult = await transformer.transformIssue(issueResp, cMeta, epicNameProvider);
+    return {
+        ...result,
+        key: issue.key,
+        id: issue.id,
+        self: issue.self,
+        siteDetails: issue.siteDetails,
+        isEpic: issue.isEpic,
+        epicChildren: issue.epicChildren,
+        apiVersion: apiVersion,
+        epicFieldInfo: epicInfo,
+    };
+}
 
-// /*
-//     These three functions handle whether our application have sort of caching
-//     for the information needed for createIssueUI and editIssueUI.
-//     For our application, we will mostly always have sort of cache information
-//     but it is meant to handle first uses of each function where we do not have any
-//     information like the first time render of a webview. After the first, the application will
-//     have a registered cached for the Atlascode. Also, since this is a public package,
-//     these functions handle input with only the client and issue passed as arguments.
-// */
+/*
+    These three functions handle whether our application have sort of caching
+    for the information needed for createIssueUI and editIssueUI.
+    For our application, we will mostly always have sort of cache information
+    but it is meant to handle first uses of each function where we do not have any
+    information like the first time render of a webview. After the first, the application will
+    have a registered cached for the Atlascode. Also, since this is a public package,
+    these functions handle input with only the client and issue passed as arguments.
+*/
 
-// async function getFieldsInfo<S extends JiraSiteInfo>(client: JiraClient<S>, fields: Fields): Promise<Fields> {
-//     if (!fields) {
-//         let allFields: Fields;
-//         allFields = {};
-//         let fetchedFields = await client.getFields();
-//         if (fetchedFields) {
-//             fetchedFields.forEach((field) => {
-//                 console.log('processing field', field);
-//                 const key = field.key ? field.key : field.id;
-//                 allFields[key] = readField(field);
-//             });
-//         }
-//         return allFields;
-//     }
-//     return fields;
-// }
+async function getFieldsInfo<S extends JiraSiteInfo>(client: JiraClient<S>, fields: Fields): Promise<Fields> {
+    if (!fields) {
+        const allFields: Fields = {};
+        const fetchedFields = await client.getFields();
+        if (fetchedFields) {
+            fetchedFields.forEach((field) => {
+                console.log('processing field', field);
+                const key = field.key ? field.key : field.id;
+                allFields[key] = readField(field);
+            });
+        }
+        return allFields;
+    }
+    return fields;
+}
 
-// async function getIssueLinkTypesInfo<S extends JiraSiteInfo>(
-//     client: JiraClient<S>,
-//     cachedIssueLinkTypes: IssueLinkType[],
-// ): Promise<IssueLinkType[]> {
-//     if (cachedIssueLinkTypes.length === 0) {
-//         return await client.getIssueLinkTypes();
-//     }
-//     return cachedIssueLinkTypes;
-// }
+async function getIssueLinkTypesInfo<S extends JiraSiteInfo>(
+    client: JiraClient<S>,
+    cachedIssueLinkTypes: IssueLinkType[],
+): Promise<IssueLinkType[]> {
+    if (cachedIssueLinkTypes.length === 0) {
+        return await client.getIssueLinkTypes();
+    }
+    return cachedIssueLinkTypes;
+}
 
-// async function getCreateIssueMetadataInfo<S extends JiraSiteInfo>(
-//     client: JiraClient<S>,
-//     projectKey: string,
-//     cachedCMeta: IssueCreateMetadata,
-// ): Promise<IssueCreateMetadata> {
-//     if (cachedCMeta.projects.length === 0) {
-//         return await client.getCreateIssueMetadata(projectKey);
-//     }
-//     return cachedCMeta;
-// }
+async function getCreateIssueMetadataInfo<S extends JiraSiteInfo>(
+    client: JiraClient<S>,
+    projectKey: string,
+    cachedCMeta: IssueCreateMetadata,
+): Promise<IssueCreateMetadata> {
+    if (cachedCMeta.projects.length === 0) {
+        return await client.getCreateIssueMetadata(projectKey);
+    }
+    return cachedCMeta;
+}
