90190459f0de4896d0b7717a1174fb770193c536
Serhii Panchenko <spanchenko@atlassian.com>
2025-07-08 16:58:41 +0200

    AXON-585: added descriptive comment

--- e2e/helpers/field-updaters/updaters.ts
+++ e2e/helpers/field-updaters/updaters.ts
@@ -3,6 +3,15 @@ import { updateAttachment } from './updateAttachment';
 import { updateComment } from './updateComment';
 import { updateDescription } from './updateDescription';
 
+/**
+ * Collection of field updaters for Jira issue fields.
+ * Each updater is a function that takes an issue object and a value, then updates the issue accordingly.
+ *
+ * To add a new field updater:
+ * 1. Create a new file updateYourField.ts
+ * 2. Implement the FieldUpdater type (see types.ts)
+ * 3. Import and add it to fieldUpdaters object with the field name as the key
+ */
 const fieldUpdaters: Record<string, FieldUpdater> = {
     attachment: updateAttachment,
     comment: updateComment,
