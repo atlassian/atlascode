1d20d5522d823033c48aaa505650ad66a88c752e
Migelangel Carrillo <migelangelcarrillo2@gmail.com>
2025-07-18 11:27:07 -0700

    Added support for Server instances
    There was no support for epics and their children for cloud instances. The cloud uses parent and child relationship while in DC instance we have the old epic and epic child relationship. Epics should now be supported for Cloud instances and DC instances versions 7.0+ (according to rovo) but I tested on 9.11 in instEnv.

--- src/webviews/components/issue/view-issue-screen/JiraIssuePage.tsx
+++ src/webviews/components/issue/view-issue-screen/JiraIssuePage.tsx
@@ -197,7 +197,15 @@ export default class JiraIssuePage extends AbstractIssueEditorPage<Emit, Accept,
                 this.setState({ isSomethingLoading: true, loadingField: field.key });
                 const payload: any = newValue;
                 payload.project = { key: this.getProjectKey() };
-                payload.parent = { key: this.state.key };
+                if (this.state.siteDetails.isCloud) {
+                    payload.parent = { key: this.state.key }; // Cloud instances have parent-child relationships for epics and non-epics
+                } else {
+                    if (this.state.isEpic) {
+                        payload[this.state.epicFieldInfo.epicLink.id] = this.state.key; // Epic children
+                    } else {
+                        payload.parent = { key: this.state.key }; // Regular subtasks
+                    }
+                }
                 this.postMessage({
                     action: 'createIssue',
                     site: this.state.siteDetails,
@@ -543,7 +551,13 @@ export default class JiraIssuePage extends AbstractIssueEditorPage<Emit, Accept,
                     linkTypes={this.state.selectFieldOptions['issuelinks']}
                     isEpic={this.state.isEpic}
                     epicChildren={this.state.epicChildren}
-                    epicChildrenTypes={this.state.selectFieldOptions['issuetype']}
+                    epicChildrenTypes={
+                        this.state.siteDetails.isCloud
+                            ? this.state.selectFieldOptions['issuetype']
+                            : this.state.selectFieldOptions['issuetype'].filter((type) => {
+                                  return type.name !== 'Epic'; // The array is size 4 by default so no perf problems, filter reduces to 3
+                              })
+                    }
                     handleInlineEdit={this.handleInlineEdit}
                     handleOpenIssue={this.handleOpenIssue}
                     onFetchIssues={async (input: string) =>
