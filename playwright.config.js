a3acda1fd8f66c0cf11d24f4e72bb80bd4593636
Marco Mura <mmura@atlassian.com>
2025-07-18 22:48:38 -0700

    Extend E2E timeouts to 50s

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    use: {
-        viewport: {
-            width: 1600,
-            height: 800,
-        },
+    timeout: 50000,
+    viewport: {
+        width: 1600,
+        height: 800,
     },
-    use: {
-        // Docs: https://playwright.dev/docs/videos
-        // To see all of the videos, change this to 'on'
-        video: 'retain-on-failure',
-    },
+
+    // Docs: https://playwright.dev/docs/videos
+    // To see all of the videos, change this to 'on'
+    video: 'retain-on-failure',
 });
