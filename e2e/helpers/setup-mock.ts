804f2d7e4b8bdd8d8b312e65358d555f0cb17a25
bhorai-atl <bhorai@atlassian.com>
2025-07-21 09:37:15 +0300

    test: added setup-mock utils

---
+++ e2e/helpers/setup-mock.ts
@@ -0,0 +1,35 @@
+import { MinimalIssue } from '@atlassianlabs/jira-pi-common-models';
+import { APIRequestContext } from '@playwright/test';
+import fs from 'fs';
+import { DetailedSiteInfo } from 'src/atlclients/authInfo';
+
+import { cleanupWireMockMapping, setupWireMockMapping } from './common';
+import { updateIssueField } from './update-jira-issue';
+
+const issueKey = 'BTS-1';
+
+export async function setupSearchMock(request: APIRequestContext, status: string) {
+    const searchJSON = JSON.parse(fs.readFileSync('e2e/wiremock-mappings/mockedteams/search.json', 'utf-8'));
+    const parsedBody = JSON.parse(searchJSON.response.body);
+    const updatedIssue = structuredClone(parsedBody);
+
+    // TODO: refactor like updateIssueField
+    const issueIndex = updatedIssue.issues.findIndex(({ key }: MinimalIssue<DetailedSiteInfo>) => key === issueKey);
+    updatedIssue.issues[issueIndex].fields.status.name = status;
+    updatedIssue.issues[issueIndex].fields.status.statusCategory.name = status;
+
+    const { id } = await setupWireMockMapping(request, 'GET', updatedIssue, '/rest/api/2/search');
+    return () => cleanupWireMockMapping(request, id);
+}
+
+export async function setupIssueMock(request: APIRequestContext, updates: Record<string, any>) {
+    const issueJSON = JSON.parse(fs.readFileSync('e2e/wiremock-mappings/mockedteams/BTS-1/bts1.json', 'utf-8'));
+
+    const { id } = await setupWireMockMapping(
+        request,
+        'GET',
+        updateIssueField(issueJSON, updates),
+        '/rest/api/2/issue/BTS-1',
+    );
+    return () => cleanupWireMockMapping(request, id);
+}
