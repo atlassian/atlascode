fdf8a6c2cd0e9c7eaa6525e815f5d80ad8d19a98
bhorai-atl <bhorai@atlassian.com>
2025-07-28 22:49:06 +0300

    test: added fragments

---
+++ e2e/page-objects/fragments/index.ts
@@ -0,0 +1,4 @@
+export { IssueComments } from './IssueComments';
+export { IssueDescription } from './IssueDescription';
+export { IssueQuickContent } from './IssueQuickContent';
+export { IssueStatus } from './IssueStatus';
