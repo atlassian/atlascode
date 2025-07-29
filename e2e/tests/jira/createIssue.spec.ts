fb8b8e2655f5c975be1dc564eef552fd69d6e3c9
bhorai-atl <bhorai@atlassian.com>
2025-07-30 00:43:42 +0300

    test: updated createIssue e2e test

--- e2e/tests/jira/createIssue.spec.ts
+++ e2e/tests/jira/createIssue.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
 import { authenticateWithJira } from 'e2e/helpers';
-import { AtlascodeDrawer, AtlassianSettings, CreateIssuePage } from 'e2e/page-objects';
+import { AppNotifications, AtlascodeDrawer, AtlassianSettings, CreateIssuePage } from 'e2e/page-objects';
 
 const NEW_ISSUE_SUMMARY = 'Test Issue Created via E2E Test';
 const NEW_ISSUE_KEY = 'BTS-7';
@@ -22,4 +22,5 @@ test('Create an issue via side panel flow', async ({ page }) => {
     await page.waitForTimeout(1_000);
 
     await createIssuePage.expectIssueCreated(NEW_ISSUE_KEY);
+    await new AppNotifications(page).expectNotification(`Issue ${NEW_ISSUE_KEY} has been created`);
 });
