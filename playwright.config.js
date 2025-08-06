8cb8e94c8f978fdc94a8186318fa6f74f8fa111c
Mykyta Bohoslavskyi <mbohoslavskyi@atlassian.com>
2025-08-06 14:40:53 +0300

    AXON-537 turned on trace on build failure

--- playwright.config.js
+++ playwright.config.js
@@ -6,10 +6,7 @@ export default defineConfig({
             width: 1600,
             height: 800,
         },
-    },
-    use: {
-        // Docs: https://playwright.dev/docs/videos
-        // To see all of the videos, change this to 'on'
         video: 'retain-on-failure',
+        trace: 'retain-on-failure',
     },
 });
