ef91d68cd018e6f7f232b583d059ab656cc45316
Oleksandr Mazepa <omazepa@atlassian.com>
2025-08-13 13:29:28 +0300

    AXON-705: Fix slider styles

--- src/react/vscode/theme/vscodeTheme.ts
+++ src/react/vscode/theme/vscodeTheme.ts
@@ -201,6 +201,48 @@ export const createVSCodeTheme = (vscStyles: VSCodeStyles): any => {
                 // MUI 5 changed background color for paper https://v5.mui.com/material-ui/migration/v5-component-changes/#change-dark-mode-background-opacity
                 styleOverrides: { root: { backgroundImage: 'unset' } },
             },
+            MuiSlider: {
+                styleOverrides: {
+                    root: {
+                        '& .MuiSlider-track': {
+                            border: 'none',
+                            height: '2px',
+                        },
+                        '& .MuiSlider-rail': {
+                            height: '2px',
+                        },
+                        '& .MuiSlider-thumb': {
+                            width: '12px',
+                            height: '12px',
+                            '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+                                boxShadow: 'inherit',
+                            },
+                            '&::before': {
+                                display: 'none',
+                            },
+                        },
+                        '& .MuiSlider-valueLabel': {
+                            lineHeight: 1.2,
+                            fontSize: 12,
+                            background: 'unset',
+                            padding: 0,
+                            width: 32,
+                            height: 32,
+                            borderRadius: '50% 50% 50% 0',
+                            backgroundColor: vscStyles.buttonBackground,
+                            transformOrigin: 'bottom left',
+                            transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+                            '&::before': { display: 'none' },
+                            '&.MuiSlider-valueLabelOpen': {
+                                transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+                            },
+                            '& > *': {
+                                transform: 'rotate(45deg)',
+                            },
+                        },
+                    },
+                },
+            },
         },
     });
 };
