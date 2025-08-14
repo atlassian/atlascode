10d8f0f548d9ee1e5b49dd3a2a513d7a1d8cf4e1
Mykyta Bohoslavskyi <mbohoslavskyi@atlassian.com>
2025-08-14 16:42:36 +0300

    cleanup

--- e2e/helpers/index.ts
+++ e2e/helpers/index.ts
@@ -1,7 +1,5 @@
 export { updateIssueField } from './update-jira-issue';
 export { authenticateWithBitbucketDC, authenticateWithBitbucketCloud } from './bitbucket-auth';
-export { connectRepository } from './bitbucket-connect-repository';
-export { createPullrequest } from './create-pr';
 export { authenticateWithJira, authenticateWithJiraDC } from './jira-auth';
 export { getIssueFrame, openAtlassianSettings, closeOnboardingQuickPick } from './common';
 export {
