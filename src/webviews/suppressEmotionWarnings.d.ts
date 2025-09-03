040bf0e808180ff8e36e6e71d7f6190a8296bf03
bhulovatyi <bhulovatyi@atlassian.com>
2025-09-03 16:46:56 +0300

    AXON-1020: Removed unnecessary code and files

--- src/webviews/suppressEmotionWarnings.d.ts
+++
@@ -1,6 +0,0 @@
-// Type declaration for suppressEmotionWarnings module
-declare module '../suppressEmotionWarnings' {
-    // This module only has side effects (console filtering)
-    const value: undefined;
-    export = value;
-}
