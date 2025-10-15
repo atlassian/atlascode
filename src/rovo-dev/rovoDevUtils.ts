8d92ef80beff2bfbf06999151038d7a0abbe47b0
Marco Mura <mmura@atlassian.com>
2025-10-15 15:26:02 -0700

    [AXON-1375, AXON-1376] Add support for /status command

--- src/rovo-dev/rovoDevUtils.ts
+++ src/rovo-dev/rovoDevUtils.ts
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
 import { window, workspace } from 'vscode';
 
+import { RovoDevStatusResponse } from './responseParserInterfaces';
+
 export type SupportedConfigFiles = '.rovodev/config.yml' | '.rovodev/mcp.json' | '.rovodev/.agent.md';
 
 export async function openRovoDevConfigFile(configFile: SupportedConfigFiles, friendlyName: string) {
@@ -30,4 +32,73 @@ export async function openRovoDevConfigFile(configFile: SupportedConfigFiles, fr
     } catch (err) {
         window.showErrorMessage(`Could not open ${friendlyName} (${filePath}): ${err}`);
     }
+}
+
+export function statusJsonResponseToMarkdown(response: RovoDevStatusResponse): string {
+    const data = response.data;
+
+    let buffer = '';
+    buffer += '**Rovo Dev**\n';
+    buffer += `- Session ID: ${data.cliVersion.sessionId}\n`;
+    buffer += `- Version: ${data.cliVersion.version}\n`;
+    buffer += '\n';
+
+    buffer += '**Working directory**\n';
+    buffer += `- ${data.workingDirectory}\n`;
+    buffer += '\n';
+
+    buffer += '**Account**\n';
+    buffer += `- Email: ${data.account.email}\n`;
+    buffer += `- Atlassian account ID: ${data.account.accountId}\n`;
+    buffer += `- Atlassian organisation ID: ${data.account.orgId}\n`;
+    buffer += '\n';
+
+    if (data.memory.hasMemoryFiles) {
+        buffer += '**Memory**\n';
+        for (const memFile of data.memory.memoryPaths) {
+            buffer += `- ${memFile}\n`;
+        }
+        buffer += '\n';
+    }
+
+    buffer += '**Model**\n';
+    buffer += `- ${data.model.humanReadableName}`;
+
+    return buffer;
+}
+
+export function JsonToMarkdown(data: Record<string, any>): string {
+    return parse(0, data);
+}
+
+function mdIndent(indent: number, text: string) {
+    return indent ? ' '.repeat(indent) + `- ${text}` : text;
+}
+
+function parse(indent: number, data: Record<string, any>): string {
+    let buffer = '';
+
+    for (const key in data) {
+        const value = data[key];
+        switch (typeof value) {
+            case 'string':
+            case 'boolean':
+            case 'number':
+            case 'bigint':
+            case 'undefined':
+                buffer += mdIndent(indent, `**${key}**: ${value}\n`);
+                break;
+
+            case 'object':
+                if (value === null) {
+                    buffer += mdIndent(indent, `**${key}**: null\n`);
+                } else {
+                    buffer += mdIndent(indent, `**${key}**:\n`);
+                    buffer += parse(indent + 2, value) + '\n';
+                }
+                break;
+        }
+    }
+
+    return buffer;
 }
