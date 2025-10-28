8f5a505967076372d69a895989ecfb6671268c43
afitzgerald00 <afitzgerald@atlassian.com>
2025-10-28 11:17:24 +1100

    add help button

--- src/commands.ts
+++ src/commands.ts
@@ -504,6 +504,9 @@ export function registerRovoDevCommands(vscodeContext: ExtensionContext) {
         commands.registerCommand(Commands.RovodevShareFeedback, () =>
             Container.rovodevWebviewProvider.executeTriggerFeedback(),
         ),
+        commands.registerCommand(Commands.RovodevOpenHelp, () => {
+            env.openExternal(Uri.parse('https://www.google.com'));
+        }),
         commands.registerCommand(Commands.RovodevAddToContext, async () => {
             const context = buildContext(window.activeTextEditor, vscodeContext);
             if (!context || context.length === 0) {
