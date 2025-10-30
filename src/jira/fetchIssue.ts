54b73aa2c834bd9cc6a771eecb5c87b7da7046ca
bhulovatyi <bhulovatyi@atlassian.com>
2025-10-30 15:52:31 +0200

    AXON-106: Added REST API v3 from pi-clients

--- src/jira/fetchIssue.ts
+++ src/jira/fetchIssue.ts
@@ -1,4 +1,5 @@
 import { createIssueUI, EditIssueUI, editIssueUI } from '@atlassianlabs/jira-metaui-client';
+import { DEFAULT_API_VERSION } from '@atlassianlabs/jira-pi-client';
 import {
     isMinimalIssue,
     MinimalIssue,
@@ -9,26 +10,19 @@ import { CreateMetaTransformerResult } from '@atlassianlabs/jira-pi-meta-models'
 
 import { DetailedSiteInfo } from '../atlclients/authInfo';
 import { Container } from '../container';
-import { Logger } from '../logger';
 import { SearchJiraHelper } from '../views/jira/searchJiraHelper';
-
-// Use API v3 for all operations
-const API_VERSION = '3';
 
 export async function fetchCreateIssueUI(
     siteDetails: DetailedSiteInfo,
     projectKey: string,
 ): Promise<CreateMetaTransformerResult<DetailedSiteInfo>> {
-    Logger.info(`[V3] fetchCreateIssueUI: Using API version ${API_VERSION} for CREATE`);
-    Logger.info(`[V3] Project: ${projectKey}, Site: ${siteDetails.baseApiUrl}`);
-
     const client = await Container.clientManager.jiraClient(siteDetails);
     const [fields, issueLinkTypes, issueCreateMetadata] = await Promise.all([
         Container.jiraSettingsManager.getAllFieldsForSite(siteDetails),
         Container.jiraSettingsManager.getIssueLinkTypes(siteDetails),
         Container.jiraSettingsManager.getIssueCreateMetadata(projectKey, siteDetails),
     ]);
-    return await createIssueUI(projectKey, client, API_VERSION, {
+    return await createIssueUI(projectKey, client, DEFAULT_API_VERSION, {
         fields,
         issueLinkTypes,
         issueCreateMetadata,
@@ -67,9 +61,6 @@ export async function fetchMinimalIssue(
 }
 
 export async function fetchEditIssueUI(issue: MinimalIssue<DetailedSiteInfo>): Promise<EditIssueUI<DetailedSiteInfo>> {
-    Logger.info(`[V3] fetchEditIssueUI: Using API version ${API_VERSION} for UPDATE`);
-    Logger.info(`[V3] Issue: ${issue.key}, Site: ${issue.siteDetails.baseApiUrl}`);
-
     const client = await Container.clientManager.jiraClient(issue.siteDetails);
     const [fields, issueLinkTypes, issueCreateMetadata] = await Promise.all([
         Container.jiraSettingsManager.getAllFieldsForSite(issue.siteDetails),
@@ -79,5 +70,5 @@ export async function fetchEditIssueUI(issue: MinimalIssue<DetailedSiteInfo>): P
             issue.siteDetails,
         ),
     ]);
-    return await editIssueUI(issue, client, API_VERSION, { fields, issueLinkTypes, issueCreateMetadata });
+    return await editIssueUI(issue, client, DEFAULT_API_VERSION, { fields, issueLinkTypes, issueCreateMetadata });
 }
