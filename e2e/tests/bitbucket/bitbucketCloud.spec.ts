adb5e1a9a1ae593c76721d47bc50a9a66fa84cbe
Bohdan Horai <bhorai@atlassian.com>
2025-11-06 17:14:46 +0200

    test: reset WireMock mappings before each test

--- e2e/tests/bitbucket/bitbucketCloud.spec.ts
+++ e2e/tests/bitbucket/bitbucketCloud.spec.ts
@@ -1,9 +1,11 @@
 import { test } from '@playwright/test';
-import { authenticateWithBitbucketCloud, closeOnboardingQuickPick } from 'e2e/helpers';
+import { authenticateWithBitbucketCloud, closeOnboardingQuickPick, resetWireMockMappings } from 'e2e/helpers';
 import { JiraTypes as BitbucketTypes } from 'e2e/helpers/types';
 import { bitbucketScenarios } from 'e2e/scenarios/bitbucket';
 
 test.describe('Bitbucket Cloud', () => {
+    test.beforeEach(resetWireMockMappings);
+
     for (const scenario of bitbucketScenarios) {
         test(scenario.name, async ({ page, context, request }) => {
             await authenticateWithBitbucketCloud(page, context);
