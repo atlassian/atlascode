7c891139a72c7621ed7760614b268a20755030d6
Olena Lymar <olymar@atlassian.com>
2025-11-24 13:02:54 +0200

    AXON-1294 - Rovo Dev webview state preservation when dragging to another panel or reloading (#1314)

--- src/rovo-dev/rovoDevWebviewProvider.ts
+++ src/rovo-dev/rovoDevWebviewProvider.ts
@@ -46,6 +46,7 @@ import {
     RovoDevEntitlementCheckFailedDetail,
     RovoDevProviderMessage,
     RovoDevProviderMessageType,
+    RovoDevWebviewState,
 } from './rovoDevWebviewProviderMessages';
 import { ModifiedFile, RovoDevViewResponse, RovoDevViewResponseType } from './ui/rovoDevViewMessages';
 import { modifyFileTitleMap } from './ui/utils';
@@ -90,6 +91,7 @@ export class RovoDevWebviewProvider extends Disposable implements WebviewViewPro
     private _debugPanelEnabled = false;
     private _debugPanelContext: Record<string, string> = {};
     private _debugPanelMcpContext: Record<string, string> = {};
+    private _savedState: RovoDevWebviewState | undefined = undefined;
 
     private _userInfo: UserInfo | undefined;
 
@@ -240,7 +242,7 @@ export class RovoDevWebviewProvider extends Disposable implements WebviewViewPro
 
     public resolveWebviewView(
         webviewView: WebviewView,
-        _context: WebviewViewResolveContext,
+        context: WebviewViewResolveContext,
         _token: CancellationToken,
     ): Thenable<void> | void {
         this._webView = webviewView.webview;
@@ -250,6 +252,10 @@ export class RovoDevWebviewProvider extends Disposable implements WebviewViewPro
 
         this._chatProvider.setWebview(webview);
         this._chatContextprovider.setWebview(webview);
+
+        if (context.state) {
+            this._savedState = context.state as RovoDevWebviewState;
+        }
 
         webview.options = {
             enableCommandUris: true,
@@ -390,6 +396,14 @@ export class RovoDevWebviewProvider extends Disposable implements WebviewViewPro
                         this._webviewReady = true;
                         this.refreshDebugPanel(true);
                         this.refreshThinkingBlock();
+
+                        if (this._savedState) {
+                            await webview.postMessage({
+                                type: RovoDevProviderMessageType.RestoreState,
+                                state: this._savedState,
+                            });
+                            this._savedState = undefined;
+                        }
 
                         if (!this.isBoysenberry) {
                             // listen to change of process state by the process manager
