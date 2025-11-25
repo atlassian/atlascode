42e32c5517fa37df67d95c6ab9b2a8e91ff88b6a
Christian Abella <cabella@atlassian.com>
2025-11-24 19:18:19 -1000

    AXON-1544: fix e2e tests 2

--- e2e/page-objects/CreateIssuePage.ts
+++ e2e/page-objects/CreateIssuePage.ts
@@ -20,7 +20,7 @@ export class CreateIssuePage {
     }
 
     async createIssue() {
-        const createButton = this.frame.getByRole('button', { name: 'Create' });
+        const createButton = this.frame.getByRole('button', { name: 'Create', exact: true });
         await createButton.scrollIntoViewIfNeeded();
         await this.page.waitForTimeout(500);
         await createButton.click();
