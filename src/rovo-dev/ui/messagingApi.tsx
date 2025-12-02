ffc9f16a5f7d7407e9318631c443b68df540a9fd
Olena Lymar <olymar@atlassian.com>
2025-12-02 13:13:48 +0200

    AXON-1294 - added setState to the new copy of useMessagingApi

--- src/rovo-dev/ui/messagingApi.tsx
+++ src/rovo-dev/ui/messagingApi.tsx
@@ -82,6 +82,13 @@ export function useMessagingApi<A, M extends ReducerAction<any, any>, R extends 
         [onMessageHandler],
     );
 
+    const setState = useCallback(
+        (state: Record<string, any>): void => {
+            apiRef.setState(state);
+        },
+        [apiRef],
+    );
+
     useEffect(() => {
         window.addEventListener('message', internalMessageHandler);
         apiRef.postMessage({ type: 'refresh' });
@@ -91,5 +98,5 @@ export function useMessagingApi<A, M extends ReducerAction<any, any>, R extends 
         };
     }, [onMessageHandler, internalMessageHandler, apiRef]);
 
-    return { postMessage, postMessagePromise };
+    return { postMessage, postMessagePromise, setState };
 }
