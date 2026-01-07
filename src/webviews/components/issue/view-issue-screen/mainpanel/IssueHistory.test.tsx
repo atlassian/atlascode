4331122e2022e8a74e20af945577a96493bb71bc
Marco Mura <mmura@atlassian.com>
2026-01-08 10:41:05 +1100

    better fix

--- src/webviews/components/issue/view-issue-screen/mainpanel/IssueHistory.test.tsx
+++ src/webviews/components/issue/view-issue-screen/mainpanel/IssueHistory.test.tsx
@@ -4,12 +4,14 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { IssueHistoryItem } from 'src/ipc/issueMessaging';
 import { disableConsole } from 'testsutil/console';
+import timezoneMock from 'timezone-mock';
 
 import { IssueHistory } from './IssueHistory';
 
 describe('IssueHistory', () => {
     beforeAll(() => {
         disableConsole('warn', 'error');
+        timezoneMock.register('US/Pacific');
     });
 
     it('renders loading state', () => {
@@ -228,7 +230,7 @@ describe('IssueHistory', () => {
         const history: IssueHistoryItem[] = [
             {
                 id: '123-status',
-                timestamp: '2025-11-05T00:00:00.000Z',
+                timestamp: '2025-11-05T15:58:00.000Z',
                 author: {
                     displayName: 'John Doe',
                     accountId: 'user-1',
