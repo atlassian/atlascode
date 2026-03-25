facc0323a046721097c35db15f756a2c5fd98e3d
Michael <mchun@atlassian.com>
2026-03-25 15:01:45 -0700

    remove testing feature

--- src/rovo-dev/api/extensionApi.ts
+++ src/rovo-dev/api/extensionApi.ts
@@ -106,9 +106,6 @@ export class ExtensionApi {
         isThinkingBlockEnabled: (): boolean => {
             return Container.config.rovodev.thinkingBlockEnabled;
         },
-        isJiraEnabled: (): boolean => {
-            return Container.config.jira.enabled;
-        },
         onDidChange: (listener: (e: ConfigurationChangeEvent) => any, thisArg?: any) => {
             return configuration.onDidChange(listener, thisArg);
         },
