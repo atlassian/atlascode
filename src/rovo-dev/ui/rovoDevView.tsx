545e6e007cc7e5f2a8c5bd1ad14e33f8031102bc
cindy-atl <cshow@atlassian.com>
2026-04-16 05:41:46 +1000

    FLOW-1938: Remove Create Pull Request button and related code from chat extension - (Rovo Dev) (#1782)
    * FLOW-1938 refactor: remove Create pull request button and related code
    
    Remove the "Create pull request" button from the chat extension as PR creation is now handled via the new Pull Request button in the Session header. This includes:
    - Deletion of the entire rovoDevPullRequestHandler and its tests
    - Removal of PullRequestForm component and its tests
    - Removal of all references to PR creation in chat UI, webview provider, chat stream, and related messaging
    - Cleanup of related state, telemetry events, and message types
    
    This simplifies the codebase by removing deprecated PR creation UI and logic.
    
    * FLOW-1938: cleanup
    
    * update lock file
    
    * revert file

--- src/rovo-dev/ui/rovoDevView.tsx
+++ src/rovo-dev/ui/rovoDevView.tsx
@@ -40,7 +40,6 @@ import {
     extractLastNMessages,
     modifyFileTitleMap,
     processDropDataTransferItems,
-    PullRequestMessage,
     Response,
     safeJsonParse,
     ToolReturnParseResult,
@@ -402,9 +401,6 @@ const RovoDevView: React.FC = () => {
                     setPromptContextCollection(event.context);
                     break;
 
-                case RovoDevProviderMessageType.CreatePRComplete:
-                case RovoDevProviderMessageType.GetCurrentBranchNameComplete:
-                case RovoDevProviderMessageType.CheckGitChangesComplete:
                 case RovoDevProviderMessageType.UpdateSavedPrompts:
                     break; // This is handled elsewhere
 
@@ -736,22 +732,6 @@ const RovoDevView: React.FC = () => {
             type: RovoDevViewResponseType.RestartProcess,
         });
     }, [postMessage]);
-
-    const onChangesGitPushed = useCallback(
-        (msg: PullRequestMessage, pullRequestCreated: boolean) => {
-            if (totalModifiedFiles.length > 0) {
-                keepFiles(totalModifiedFiles);
-            }
-
-            handleAppendResponse(msg);
-
-            postMessage({
-                type: RovoDevViewResponseType.ReportChangesGitPushed,
-                pullRequestCreated,
-            });
-        },
-        [totalModifiedFiles, handleAppendResponse, postMessage, keepFiles],
-    );
 
     const onCollapsiblePanelExpanded = useCallback(() => {
         postMessage({
@@ -1105,7 +1085,6 @@ const RovoDevView: React.FC = () => {
                         pendingSubagentTasks={pendingSubagentTasks}
                         deepPlanCreated={deepPlanCreated}
                         currentState={currentState}
-                        onChangesGitPushed={onChangesGitPushed}
                         onCollapsiblePanelExpanded={onCollapsiblePanelExpanded}
                         handleFeedbackTrigger={handleFeedbackTrigger}
                         onLoginClick={onLoginClick}
