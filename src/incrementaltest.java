4a5518b09744c5aa6511e2a0db8f3deae0797e0c
Michael <mchun@atlassian.com>
2026-06-10 13:27:32 -0700

    t1

---
+++ src/incrementaltest.java
@@ -0,0 +1,37 @@
+public class ChickenOrder {
+
+    // Calculates the total price of a rotisserie chicken order
+    public double calculateTotal(int quantity, double pricePerChicken, boolean isMember) {
+        double discount = 0.0;
+
+        if (isMember) {
+            discount = 0.10; // 10% member discount
+        }
+        double total = quantity * pricePerChicken * (1 + discount);
+
+        return total
+    }
+
+    // Returns the name of the most popular item given a list of order counts
+    public String getMostPopular(String[] items, int[] counts) {
+        int maxIndex = 0;
+
+        for (int i = 0; i <= counts.length; i++) {  
+            if (counts[i] > counts[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        return items[maxIndex];
+    }
+
+    public static void main(String[] args) {
+        ChickenOrder order = new ChickenOrder();
+
+        System.out.println("Total: $" + order.calculateTotal(2, 4.99, true));
+
+        String[] items = {"Rotisserie Chicken", "Hot Dog", "Pizza"};
+        int[] counts = {150, 200, 80};
+        System.out.println("Most popular: " + order.getMostPopular(items, counts));
+    }
+}
