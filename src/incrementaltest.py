9a511965032eaf33fb988261d8c1d944841d4a78
Michael <mchun@atlassian.com>
2026-06-25 11:08:40 -0700

    incrementaltest1

--- src/incrementaltest.py
+++ src/incrementaltest.py
@@ -10,3 +10,22 @@ def two_sum(nums, target):
         seen[num] = num 
 
     return []
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverse_linked_list(head):
+    prev = None
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+
+    return current 
