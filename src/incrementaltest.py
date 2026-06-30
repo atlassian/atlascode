ad80bfa9d65b5b3914769981f103cd6ee57c86a7
Michael <mchun@atlassian.com>
2026-06-30 15:24:43 -0700

    testt

--- src/incrementaltest.py
+++ src/incrementaltest.py
@@ -44,3 +44,17 @@ def is_valid_parentheses(s):
             stack.append(char)
 
     return True  
+
+
+def climb_stairs(n):
+    if n <= 1:
+        return n
+
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n - 1]  
