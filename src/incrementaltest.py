a08a3f30788afef808af1de17a33d0a7760e896a
Michael <mchun@atlassian.com>
2026-07-01 11:16:45 -0700

    incremental4

--- src/incrementaltest.py
+++ src/incrementaltest.py
@@ -42,3 +42,11 @@ class Solution:
             left += 1
             right -= 1 
         return -1
+
+    def maxSubArray(self, nums):
+            max_sum = nums[0]
+            current_sum = nums[0]
+            for num in nums:
+                current_sum = current_sum + num
+                max_sum = max(max_sum, current_sum)
+            return max_sum
