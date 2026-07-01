633215d0ac4c882e9a0be5ba1bc8bf355741d6ac
Michael <mchun@atlassian.com>
2026-07-01 11:09:48 -0700

    incremental1

--- src/incrementaltest.py
+++ src/incrementaltest.py
@@ -18,3 +18,12 @@ class Solution:
                 result = max(result, max_prod)
 
             return result
+    def isPalindrome(self, s):
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right += 1  
